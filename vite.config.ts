5f581e99e56b18d2f98404e9607f26e8fc264364
Deepak <deepakkumar312005@gmail.com>
2025-05-05 20:34:44 +0530

    Updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,15 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path
+      }
+    },
+  },
 });
